ce8eb07bf4826c14900a723dc7182e9c8e4e299c
rizwan <rizwansohal001@gmail.com>
2021-03-29 15:04:14 +0500

    Task-2-AsyncAwait

--- scraper.js
+++ scraper.js
@@ -2,23 +2,20 @@ const fetch = require("node-fetch");
 const got = require("got");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const isAbsoluteUrl = require("is-absolute-url");
-var urlExists = require("url-exists");
 
 exports.scrap = async function (req, res) {
   const addressArray = req.query.address;
+  const titles = [];
   for (const address of addressArray) {
     try {
       const response = await got(address);
       let dom = new JSDOM(response.body);
       let title = dom.window.document.querySelector("title").textContent;
-      //Create HTML response here
-      console.log(title);
-      res.write(`<h1>${title}</h1>`);
+      titles.push(title);
     } catch (error) {
-      res.code(400).send("Error while fetching Title || URL is invalid");
+      res.send("Error while fetching Title || URL is invalid");
     }
   }
 
-  res.end("Hello World");
+  res.render("titles", { titles });
 };
